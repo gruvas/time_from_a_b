149cb5dc36cd29bed6f7886aa2c6aee2dbb6202d
gruvas <gruvas67@gmail.com>
2022-10-19 10:13:45 +0300

    mini fix

--- src/component/validation/number_validation.js
+++ src/component/validation/number_validation.js
@@ -1,12 +1,11 @@
 // Вызывается при вводе в input символов
 // Блокирует все символы кроме цифр
 const number_validation = (e) => {
-    let theEvent = e
-    let key_number = theEvent.keyCode || theEvent.which
+    let key_number = e.keyCode || e.which
     let key_string = String.fromCharCode( key_number )
     let regex = /[0-9]/
     if( !regex.test(key_string) ) {
-        if(theEvent.preventDefault) theEvent.preventDefault()
+        if(e.preventDefault) e.preventDefault()
     }
 }
 
